243f88a8d0c7ce02d7fa6ae93ae39ace7b90d543
alexacevedo <alexacevedo@boisestate.edu>
2020-11-01 20:59:09 -0700

    Project1 Completed

--- Projects/Project1/Project1/PhotoViewController.swift
+++ Projects/Project1/Project1/PhotoViewController.swift
@@ -6,16 +6,14 @@
 //
 
 import UIKit
-import CoreLocation
-import MapKit
 
-class PhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, CLLocationManagerDelegate {
+
+class PhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
   
     @IBOutlet weak var imageView: UIImageView!
 
     let imagePickerController = UIImagePickerController()
 
-    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,32 +21,23 @@ class PhotoViewController: UIViewController, UIImagePickerControllerDelegate, UI
         imagePickerController.allowsEditing = true
 
     }
-
-    override func viewDidAppear(_ animated: Bool) {
-        let alertController = UIAlertController(title: "Choose a profile photo", message: "Choose from your library or camera to select a profile photo to use in your new account.", preferredStyle: .actionSheet)
-
-        alertController.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: { (alertAction) in
-            print("library")
-            self.imagePickerController.sourceType = .photoLibrary
-            self.present(self.imagePickerController, animated: true, completion: nil)
-        }))
-
-        alertController.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (alertAction) in
-            print("camera")
-            if( UIImagePickerController.isSourceTypeAvailable(.camera) ){
-                self.imagePickerController.sourceType = .camera
-                self.present(self.imagePickerController, animated: true, completion: nil)
-            }
-        }))
-
-        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (alertAction) in
-            print("cancel")
-        }))
-
-        present(alertController, animated: true, completion: nil)
+    
+    @IBAction func photoLibButton(_ sender: Any) {
+        self.imagePickerController.sourceType = .photoLibrary
+        self.present(self.imagePickerController, animated: true, completion: nil)
     }
-
-
+    
+    
+    
+    @IBAction func cameraButton(_ sender: Any) {
+        
+        if( UIImagePickerController.isSourceTypeAvailable(.camera) ){
+            self.imagePickerController.sourceType = .camera
+            self.present(self.imagePickerController, animated: true, completion: nil)
+        }
+        
+    }
+    
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true, completion: nil)
     }
@@ -62,4 +51,5 @@ class PhotoViewController: UIViewController, UIImagePickerControllerDelegate, UI
     }
 
 }
+
 
